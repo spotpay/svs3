c7f4195534d5699f25e457d58c3299fcd6514c04
kale <kale@Musicanes-MacBook-Pro.local>
2009-10-07 13:00:07 -0700

    these files are no longer required now that we have our Rakefile

--- startSelenium.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-start java -jar "C:\Program Files\selenium-remote-control-1.0.1-dist\selenium-remote-control-1.0.1\selenium-server-1.0.1\selenium-server.jar"
--- start_selenium.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/sh
-java -jar $SELENIUM_JAR
